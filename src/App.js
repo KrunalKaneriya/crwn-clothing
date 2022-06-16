89f20f9b5f8bbd542c4f40065694ad18b50e8f04
Krunal Kaneria <prokillerkk@outlook.com>
2022-06-16 17:15:25 +0530

    integrate authentication with firebase and user state and route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React from 'react';
-import { Route, Switch} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
-import ShopPage from "./pages/shop/shop.component.jsx";
-import Header from "./components/header/header.component.jsx";
-
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
+import Authentication from './pages/authentication/authentication.component.jsx';
+import { auth } from './firebase/firebase.utils.js';
 import './App.css';
 
-function App() {
-	return (
-		<div>
-				<Header/>
+class App extends Component {
+
+	constructor() {
+		super();
+
+		this.state = {
+			currentUser:null
+		}
+	}
+
+	unsubscribeFromAuth = null;
+
+	componentDidMount() {
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+			this.setState({currentUser : user})
+		})
+	}
+
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
+
+	render() {
+		return (
+			<div>
+				<Header currentUser = {this.state.currentUser} />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
-					<Route exact path= "/shop" component={ShopPage}/>
+					<Route exact path="/shop" component={ShopPage} />
+					<Route exact path="/signin" component={Authentication} />
 				</Switch>
 			</div>
-	);
+		);
+	}
 }
 
 export default App;
