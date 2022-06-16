ae12d89dcb51e27864e1c1c88d67dbe913556e14
Krunal Kaneria <prokillerkk@outlook.com>
2022-06-16 17:17:11 +0530

    create auth component

---
+++ src/pages/authentication/authentication.component.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import SignIn from "../../components/sign-in/sign-in.component";
+import "./authentication.component.scss";
+
+const Authentication = () => (
+    <div className = "sign-in-and-sign-up"><SignIn/></div>
+)
+
+export default Authentication;
