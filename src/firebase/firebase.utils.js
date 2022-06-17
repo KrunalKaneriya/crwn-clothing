316e5b5672e38285f2327e351233bc69f09652ed
Krunal Kaneria <prokillerkk@outlook.com>
2022-06-17 13:42:56 +0530

    add createUserProfileDocument function which gets or sets user object

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,6 +15,30 @@ firebase.initializeApp(config);
 export const auth = firebase.auth(firebase);
 export const firestore = firebase.firestore();
 
+export const createUserProfileDocument = async (userAuth,additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const {displayName,email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt
+            })
+        } catch(error) {
+            console.log(`Error creating new user ${error}`)
+        }
+    }
+
+    return userRef;
+}
+
 const provider = new firebase.auth.GoogleAuthProvider();
 
 provider.setCustomParameters({prompt:"select_account"});
