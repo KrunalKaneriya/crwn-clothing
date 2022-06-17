5131320b4137cd49efbe036ebb7edb4df52f9099
Krunal Kaneria <prokillerkk@outlook.com>
2022-06-17 13:41:47 +0530

    add signup component

--- src/pages/authentication/authentication.component.jsx
+++ src/pages/authentication/authentication.component.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import SignIn from "../../components/sign-in/sign-in.component";
-import "./authentication.component.scss";
+import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+import './authentication.component.scss';
 
 const Authentication = () => (
-    <div className = "sign-in-and-sign-up"><SignIn/></div>
-)
+	<div className="sign-in-and-sign-up">
+		<SignIn />
+        <SignUp/>
+	</div>
+);
 
 export default Authentication;
